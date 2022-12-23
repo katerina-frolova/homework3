3d982bb9209628659d9ef0f5093d1ffb48bc5754
Екатерина <kate.frolova.16@gmail.com>
2022-12-23 16:36:58 +0300

    Сделано ДЗ3

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+
+
 
     }
 
@@ -49,6 +56,79 @@ public class Main {
         short onePupil= (short) (lists/allPupils);
         System.out.println("На каждого ученика рассчитано "+onePupil+" листов бумаги");
 
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+        byte bottlesIn2Min=16;
+        System.out.println("За 2 минуты машина произвела "+bottlesIn2Min+" штук бутылок");
+        byte bottlesInMin=(byte)(bottlesIn2Min/2);
+        short bottlesIn20Min=(short)(bottlesInMin*20);
+        System.out.println("За 20 минут машина произвела "+bottlesIn20Min+" штук бутылок");
+        short bottlesInHour=(short)(bottlesInMin*60);
+        short bottlesInDay=(short)(bottlesInHour*24);
+        System.out.println("За сутки машина произвела "+bottlesInDay+" штук бутылок");
+        System.out.println("За 3 дня машина произвела "+bottlesInDay*3+" штук бутылок");
+        int bottlesInMonth=bottlesInDay*31;
+        System.out.println("За месяц(например, декабрь) машина произвела "+bottlesInMonth+" штук бутылок");
 
     }
+    public static void task5 () {
+        System.out.println("Задача 5");
+       byte whitePaint=2;
+       byte brownPaint=4;
+       byte paint=(byte)(whitePaint+brownPaint);
+       byte allPaint=120;
+       byte classrooms=(byte)(allPaint/paint);
+       whitePaint=(byte)(whitePaint*classrooms);
+       brownPaint=(byte)(brownPaint*classrooms);
+        System.out.println("В школе, где "+classrooms+" классов, нужно "+whitePaint+" банок белой краски и "+brownPaint+" банок коричневой краски");
+
+    }
+    public static void task6 () {
+        System.out.println("Задача 6");
+        int banana=80;
+        int milk=105;
+        int icecream=100;
+        int egg=70;
+        double breakfast=banana*5+milk*2+icecream*2+egg*4;
+        System.out.println("Вес спортзавтрака составил "+breakfast+" грамм");
+        double breakfastKg=breakfast/1000;
+        System.out.println("Вес спортзавтрака составил "+breakfastKg+" килограмм");
+
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
+        double purpose=7.0;
+        double diet1=0.25;
+        double diet2=0.5;
+        int days1=(int)(purpose/diet1);
+        int days2=(int)(purpose/diet2);
+        System.out.println(days1+" дней уйдет на похудение, если спортсмен будет терять по 250г в день");
+        System.out.println(days2+" дней уйдет на похудение, если спортсмен будет терять по 500г в день");
+        int days=(int)(days1+days2)/2;
+        System.out.println(days+" день в средем уйдет на похудение");
+
+    }
+    public static void task8 () {
+        System.out.println("Задача 8");
+        int mashaSalary1=67760;
+        int mashaRise=mashaSalary1/10;
+        int mashaSalary2=mashaSalary1+mashaRise;
+        int mashaYearRise=mashaRise*12;
+        System.out.println("Маша теперь получает "+mashaSalary2+" рублей. Годовой доход вырос на "+mashaYearRise+" рублей");
+        int denisSalary1=83690;
+        int denisRise=denisSalary1/10;
+        int denisSalary2=denisSalary1+denisRise;
+        int denisYearRise=denisRise*12;
+        System.out.println("Денис теперь получает "+denisSalary2+" рублей. Годовой доход вырос на "+denisYearRise+" рублей");
+        int christinaSalary1=76230;
+        int christinaRise=christinaSalary1/10;
+        int christinaSalary2=christinaSalary1+christinaRise;
+        int christinaYearRise=christinaRise*12;
+        System.out.println("Кристина теперь получает "+christinaSalary2+" рублей. Годовой доход вырос на "+christinaYearRise+" рублей");
+
+
+
+    }
+
 }
